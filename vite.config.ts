1bf5af72875080dae1cea3381217b603da30bb49
levy nguti <nguti2003@gmail.com>
2025-06-13 17:23:36 +0300

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -5,6 +5,7 @@ import { componentTagger } from "lovable-tagger";
 
 // https://vitejs.dev/config/
 export default defineConfig(({ mode }) => ({
+  base: "/tujue-ukulima-connect/", // added this line
   server: {
     host: "::",
     port: 8080,
